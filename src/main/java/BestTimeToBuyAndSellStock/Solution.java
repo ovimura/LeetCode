2748737140edfb9fa2591156421cb053f9749944
root <root@instance-1.us-west1-b.c.true-entropy-272316.internal>
2020-04-06 05:24:17 +0000

    adding solution for Best Time To Buy And Sell Stock

--- src/main/java/BestTimeToBuyAndSellStock/Solution.java
+++ src/main/java/BestTimeToBuyAndSellStock/Solution.java
@@ -2,6 +2,38 @@ package BestTimeToBuyAndSellStock;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        return 0;
+        if(prices.length == 1)
+            return 0;
+        int profit = 0;
+        for(int i=0; i<prices.length; i++) {
+            while(i<prices.length-1 && prices[i]>=prices[i+1])
+                i++;
+            if(i == prices.length-1)
+                return profit;
+            int b = prices[i];
+            while(i<prices.length-1 && prices[i+1]>=prices[i])
+                i++;
+            int s = prices[i];
+            profit += s-b;
+
+        }
+        return profit;
+    }
+
+    public int maxProfit1(int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
     }
 }
--- src/test/java/bestTimeToBuyAndSellStock/SolutionTest.java
+++ src/test/java/bestTimeToBuyAndSellStock/SolutionTest.java
@@ -10,4 +10,28 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertEquals(0,s.maxProfit(new int[]{}));
     }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        Assert.assertEquals(7,s.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        Assert.assertEquals(4,s.maxProfit(new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        Assert.assertEquals(0,s.maxProfit(new int[]{7,6,4,3,1}));
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        Assert.assertEquals(4,s.maxProfit(new int[]{1,2,3,4,5}));
+    }
 }
