aa8a7aea0b7add8faa2e22ae9ba255971ecccd60
Ovidiu Mura <ovidiu.mura@yahoo.com>
2020-04-07 07:43:49 +0000

    adding new solution for groups anagrams

--- src/main/java/GroupAnagrams/Solution.java
+++ src/main/java/GroupAnagrams/Solution.java
@@ -1,11 +1,21 @@
 package GroupAnagrams;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> lists = new ArrayList<>();
-        return null;
+        if(strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String k = String.valueOf(chars);
+            if(!map.containsKey(k)) {
+                map.put(k, new ArrayList<>());
+            }
+            map.get(k).add(s);
+        }
+        return new ArrayList<>(map.values());
     }
 }
