63cf9d1df38aaf95eb8440b14f4f5fb717fc8206
Ovidiu Mura <mura@pdx.edu>
2020-04-15 12:39:47 -0700

    adding optimization solution for Two Sum problem

--- src/main/java/TwoSum/Solution.java
+++ src/main/java/TwoSum/Solution.java
@@ -1,5 +1,8 @@
 package TwoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int sum;
@@ -15,5 +18,29 @@ public class Solution {
             }
         }
         return new int[]{a, b};
+    }
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for(int j=0; j<nums.length; j++) {
+            int complement = target - nums[j];
+            if(map.containsKey(complement) && map.get(complement) != j) {
+                return new int[] {j,map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("There are no two sum");
+    }
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int j=0; j<nums.length; j++) {
+            int complement = target - nums[j];
+            if(map.containsKey(complement) && map.get(complement) != j) {
+                return new int[] {map.get(complement),j};
+            }
+            map.put(nums[j], j);
+        }
+        throw new IllegalArgumentException("There are no two sum");
     }
 }
--- src/test/java/twoSum/SolutionTest.java
+++ src/test/java/twoSum/SolutionTest.java
@@ -10,4 +10,15 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertArrayEquals(new int[]{0,1}, s.twoSum(new int[]{2, 7, 11, 15}, 9));
     }
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,1}, s.twoSum1(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,1}, s.twoSum2(new int[]{2, 7, 11, 15}, 9));
+    }
 }
