07f93c92863922b1bd8a68bf41c7c7e32a7d2afe
Ovidiu Mura <mura@pdx.edu>
2020-06-24 16:31:23 -0700

    adding Python 3 solution for problem Shuffle The Array

---
+++ py/README.md
@@ -0,0 +1,3 @@
+# LeetCode
+
+Solutions in Python 3 for problems in https://leetcode.com
---
+++ py/sol/P1470-ShuffleTheArray.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        li = [];
+        j = 0;
+        for i in range(0, len(nums), 2):
+            li.append(nums[j])
+            li.append(nums[n+j])
+            j += 1
+        return li
+
+
+def main():
+    s = Solution()
+    nums = [2, 5, 1, 3, 4, 7]
+    result = s.shuffle(nums, 3)
+    expected_output = [2, 3, 5, 4, 1, 7]
+    assert result == expected_output
+    failed = False
+    for i in range(0, len(nums)):
+        if result[i] != expected_output[i]:
+            failed = True
+    if failed:
+        print("Test for Solution Implementation Failed!")
+    else:
+        print("Test for Solution Implementation Passed!")
+
+
+if __name__ == "__main__":
+    main()
+
