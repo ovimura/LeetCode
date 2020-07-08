a52a2d77eec682d5d2297fee444484b4f274dd6a
Ovidiu Mura <mura@pdx.edu>
2020-07-07 17:14:58 -0700

    adding second solutions for the problem 1365

--- src/main/java/HowManyNumbersAreSmallerThanTheCurrentNumber/Solution.java
+++ src/main/java/HowManyNumbersAreSmallerThanTheCurrentNumber/Solution.java
@@ -1,5 +1,7 @@
 package HowManyNumbersAreSmallerThanTheCurrentNumber;
 
+import java.util.Arrays;
+
 public class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] result = new int[nums.length];
@@ -11,5 +13,23 @@ public class Solution {
             }
         }
         return result;
+    }
+
+    public int[] smallerNumbersThanCurrent1(int[] nums) {
+        int max = 0;
+        for(int i=0; i<nums.length; i++)
+            if(nums[i]>max)
+                max = nums[i];
+        int[] result = new int[max+1];
+        for(int i=0; i<nums.length; i++)
+            result[nums[i]]++;
+        for(int i=1; i<result.length; i++) {
+            result[i] += result[i-1];
+        }
+        int[] temp = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            temp[i] = result[nums[i]-1];
+        }
+        return temp;
     }
 }
--- src/test/java/howManyNumbersAreSmallerThanTheCurrentNumber/SolutionTest.java
+++ src/test/java/howManyNumbersAreSmallerThanTheCurrentNumber/SolutionTest.java
@@ -22,4 +22,22 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertArrayEquals(new int[]{0,0,0,0}, s.smallerNumbersThanCurrent(new int[]{7,7,7,7}));
     }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,0,0,0}, s.smallerNumbersThanCurrent1(new int[]{7,7,7,7}));
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{}, s.smallerNumbersThanCurrent1(new int[]{}));
+    }
+
+    @Test
+    public void test6() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{4,0,1,1,3}, s.smallerNumbersThanCurrent1(new int[]{8,1,2,2,3}));
+    }
 }
