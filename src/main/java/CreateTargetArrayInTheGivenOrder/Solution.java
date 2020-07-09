91c5e04b0407620513ed12d27084dc3f3d94d26c
Ovidiu Mura <mura@pdx.edu>
2020-07-09 11:41:20 -0700

    adding solution to the problem Create Target Array In The Given Order

--- src/main/java/CreateTargetArrayInTheGivenOrder/Solution.java
+++ src/main/java/CreateTargetArrayInTheGivenOrder/Solution.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
+
     public int[] createTargetArray(int[] nums, int[] index) {
         int[] result = new int[nums.length];
         Arrays.fill(result, -1);
@@ -28,6 +29,37 @@ public class Solution {
                 }
             }
             it.clear();
+        }
+        return result;
+    }
+
+    public int[] createTargetArray1(int[] nums, int[] index) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        for(int i=0; i<nums.length; i++) {
+            if(result[index[i]] != -1)
+                shift(result, index[i]);
+            result[index[i]] = nums[i];
+        }
+        return result;
+    }
+
+    public void shift(int[] re, int idx) {
+        for(int j = re.length-2; j>=idx; j--) {
+            re[j+1] = re[j];
+        }
+    }
+
+    public int[] createTargetArray2(int[] nums, int[] index) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        for(int i=0; i<nums.length; i++) {
+            if(result[index[i]] != -1) {
+                for(int j = result.length-2; j>=index[i]; j--) {
+                    result[j+1] = result[j];
+                }
+            }
+            result[index[i]] = nums[i];
         }
         return result;
     }
--- src/test/java/createTargetArrayInTheGivenOrder/SolutionTest.java
+++ src/test/java/createTargetArrayInTheGivenOrder/SolutionTest.java
@@ -22,4 +22,40 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertArrayEquals(new int[]{1}, s.createTargetArray(new int[]{1}, new int[]{0}));
     }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{1}, s.createTargetArray1(new int[]{1}, new int[]{0}));
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,4,1,3,2}, s.createTargetArray1(new int[]{0,1,2,3,4}, new int[]{0,1,2,2,1}));
+    }
+
+    @Test
+    public void test6() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,1,2,3,4}, s.createTargetArray1(new int[]{1,2,3,4,0}, new int[]{0,1,2,3,0}));
+    }
+
+    @Test
+    public void test7() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{1}, s.createTargetArray2(new int[]{1}, new int[]{0}));
+    }
+
+    @Test
+    public void test8() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,4,1,3,2}, s.createTargetArray2(new int[]{0,1,2,3,4}, new int[]{0,1,2,2,1}));
+    }
+
+    @Test
+    public void test9() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{0,1,2,3,4}, s.createTargetArray2(new int[]{1,2,3,4,0}, new int[]{0,1,2,3,0}));
+    }
 }
