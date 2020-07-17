8d3a1c2450d87852ac5a76c5f4c8bfa20a1edf3a
Ovidiu Mura <mura@pdx.edu>
2020-07-17 09:25:37 -0700

    adding solution for problem 709 - To Lower Case

--- py/sol/ToLowerCase.py
+++ py/sol/ToLowerCase.py
@@ -1,1 +1,16 @@
-def toLowerCase(self, str: str) -> str:
+def toLowerCase(str: str) -> str:
+    s = ''
+    for i in range(len(str)):
+        if 91 > ord(str[i]) > 64:
+            s += chr(ord(str[i]) + 32)
+        else:
+            s += str[i]
+    return s
+
+
+assert toLowerCase('AGG') == 'agg'
+assert toLowerCase('Hello') == 'hello'
+assert toLowerCase("al&phaBET") == "al&phabet"
+
+print(toLowerCase('AGG'))
+
