ce57576d341bc9081169508d1ecd29a8bd8eb7d6
Ovidiu Mura <ovidiu.mura@yahoo.com>
2020-08-14 22:39:27 -0700

    adding solution for the problem - finalPricesWithASpecialDiscountInAShop

--- src/main/java/FinalPricesWithASpecialDiscountInAShop/Solution.java
+++ src/main/java/FinalPricesWithASpecialDiscountInAShop/Solution.java
@@ -2,6 +2,16 @@ package FinalPricesWithASpecialDiscountInAShop;
 
 public class Solution {
     public int[] finalPrices(int[] prices) {
-        return new int[1];
+        int[] res = new int[prices.length];
+        for(int i=0; i<prices.length; i++) {
+            res[i] = prices[i];
+            for(int j=i+1; j<prices.length; j++) {
+                if(prices[j] <= prices[i]) {
+                    res[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return res;
     }
 }
--- src/test/java/FinalPricesWithASpecialDiscountInAShop/SolutionTest.java
+++ src/test/java/FinalPricesWithASpecialDiscountInAShop/SolutionTest.java
@@ -8,6 +8,18 @@ public class SolutionTest {
     @Test
     public void testFinalPrices() {
         Solution s = new Solution();
-        Assert.assertArrayEquals(new int[1], s.finalPrices(new int[1]));
+        Assert.assertArrayEquals(new int[]{4,2,4,2,3}, s.finalPrices(new int[]{8,4,6,2,3}));
+    }
+
+    @Test
+    public void testFinalPrices1() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{1,2,3,4,5}, s.finalPrices(new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void testFinalPrices2() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{9,0,1,6}, s.finalPrices(new int[]{10,1,1,6}));
     }
 }
