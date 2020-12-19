7629b2b9b1d26bdadd229c1ab8fe78cb8bb0b265
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2020-12-18 17:42:35 -0800

    adding solution for the N-ary Tree Postorder Traversal

--- src/main/java/NaryTreePostorderTraversal/Solution.java
+++ src/main/java/NaryTreePostorderTraversal/Solution.java
@@ -1,15 +1,12 @@
 package NaryTreePostorderTraversal;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 // https://stackoverflow.com/questions/2969033/recursive-breadth-first-travel-function-in-java-or-c
 // https://www.netjstech.com/2019/03/binary-tree-traversal-breadth-first-serach-link-order-java.html
 public class Solution {
 
-    public List<Integer> postorder(Node root) {
+    public List<Integer> postorder2(Node root) {
         List<Integer> ints = new ArrayList<>();
         post(root);
         return new ArrayList();
@@ -52,19 +49,147 @@ public class Solution {
     public void post2(Node r) {
         boolean v[] = new boolean[10000];
         LinkedList<Node> l = new LinkedList();
+        LinkedList<Node> reversed = new LinkedList();
         l.add(r);
         v[r.val] = true;
         while(l.size() != 0) {
             r = l.poll();
-            System.out.print(r.val + " ");
+            reversed.add(r);
             if(r.children != null) {
-                r.children.forEach(n -> {
-                    if(v[n.val] != true) {
-                        l.add(n);
-                        v[n.val] = true;
+                for(int i=r.children.size()-1; i>=0; i--) {
+                    if(v[r.children.get(i).val] != true) {
+                        l.add(r.children.get(i));
+                        v[r.children.get(i).val] = true;
                     }
-                });
+                }
             }
         }
+        String output = "";
+        for(int j=reversed.size()-1; j>= 0; j--) {
+            output = output.concat(Integer.toString(reversed.get(j).val) + ",");
+        }
+        System.out.print(output.substring(0, output.length()-1));
     }
+
+    public void post3(Node r) {
+        if(r == null)
+            return;
+        if(r.children == null)
+            return;
+        else
+        for(int i=0; i<r.children.size(); i++) {
+            post3(r.children.get(i));
+        }
+        System.out.print(r.val);
+    }
+
+    /*
+    * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+    * Actual: [14,11,12,13,6,7,8,9,10,2,3,4,5,1]
+    * Expected: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+    * */
+    public List<Integer> postorder1(Node root) {
+        boolean v[] = new boolean[10000];
+        LinkedList<Node> l = new LinkedList();
+        LinkedList<Node> reversed = new LinkedList();
+        l.add(root);
+        v[root.val] = true;
+        while(l.size() != 0) {
+            root = l.poll();
+            reversed.add(root);
+            if(root.children != null) {
+                for(int i=root.children.size()-1; i>=0; i--) {
+                    if(v[root.children.get(i).val] != true) {
+                        l.add(root.children.get(i));
+                        v[root.children.get(i).val] = true;
+                    }
+                }
+            }
+        }
+        List<Integer> output = new ArrayList<>();
+        for(int j=reversed.size()-1; j>= 0; j--) {
+            output.add(reversed.get(j).val);
+        }
+        return output;
+    }
+
+    void dfs_util(Node root, boolean[] v) {
+        v[root.val] = true;
+
+        if(root.children != null)
+            for(int i=0; i< root.children.size(); i++) {
+                if(!v[root.children.get(i).val]) {
+                    dfs_util(root.children.get(i), v);
+                }
+            }
+        System.out.print(root.val + " ");
+    }
+
+    public void post4(Node r) {
+        boolean v[] = new boolean[10000];
+        dfs_util(r, v);
+    }
+
+    void dfs_alg(Node root, boolean[] v, List<Integer> rs) {
+        if(root == null)
+            return;
+        v[root.val] = true;
+        if(root.children != null)
+            for(int i=0; i< root.children.size(); i++) {
+//                if(!v[root.children.get(i).val]) {
+                    dfs_alg(root.children.get(i), v, rs);
+  //              }
+            }
+        System.out.print(root.val + " ");
+        rs.add(root.val);
+    }
+
+    public List<Integer> postorder3(Node r) {
+        boolean v[] = new boolean[10000];
+        List<Integer> result = new ArrayList<>();
+        dfs_alg(r, v, result);
+        result.forEach(n -> {
+            System.out.print(n);
+        });
+        return result;
+    }
+
+    void dfs_trv(Node root, List<Integer> rs) {
+        if(root == null)
+            return;
+        if(root.children != null)
+            for(int i=0; i< root.children.size(); i++) {
+                dfs_trv(root.children.get(i), rs);
+            }
+        //System.out.print(root.val + " ");
+        rs.add(root.val);
+    }
+
+    public List<Integer> postorder0(Node r) {
+        List<Integer> result = new ArrayList<>();
+        dfs_trv(r, result);
+        result.forEach(n -> {
+            System.out.print(n);
+        });
+        return result;
+    }
+
+
+    // Recursive solution
+    void traverse(Node root, List<Integer> rs) {
+        if(root == null)
+            return;
+        if(root.children != null)
+            for(int i=0; i< root.children.size(); i++) {
+                traverse(root.children.get(i), rs);
+            }
+        rs.add(root.val);
+    }
+
+    public List<Integer> postorder(Node r) {
+        List<Integer> result = new ArrayList<>();
+        traverse(r, result);
+        return result;
+    }
+
 }
--- src/test/java/naryTreePostorderTraversal/SolutionTest.java
+++ src/test/java/naryTreePostorderTraversal/SolutionTest.java
@@ -1,9 +1,11 @@
 package naryTreePostorderTraversal;
 
 import NaryTreePostorderTraversal.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class SolutionTest {
@@ -19,7 +21,62 @@ public class SolutionTest {
         root.children.get(0).children = new ArrayList<>();
         root.children.get(0).children.add(new Node(5));
         root.children.get(0).children.add(new Node(6));
-        //s.postorder(root);
-        s.post2(root);
+        s.postorder(root);
+        List<Integer> ls = s.postorder(root);
+        Integer[] ints = new Integer[ls.size()];
+        ints = ls.toArray(ints);
+        Assert.assertArrayEquals(new Integer[]{5, 6, 3, 2, 4, 1}, ints);
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        Node root = new Node(1);
+        root.children = new ArrayList<>();
+        root.children.add(new Node(2));
+        root.children.add(new Node(3));
+        root.children.add(new Node(4));
+        root.children.add(new Node(5));
+        root.children.get(1).children = new ArrayList<>();
+        root.children.get(1).children.add(new Node(6));
+        root.children.get(1).children.add(new Node(7));
+        root.children.get(1).children.get(1).children = new ArrayList<>();
+        root.children.get(1).children.get(1).children.add(new Node(11));
+        root.children.get(1).children.get(1).children.get(0).children = new ArrayList<>();
+        root.children.get(1).children.get(1).children.get(0).children.add(new Node(14));
+        root.children.get(2).children = new ArrayList<>();
+        root.children.get(2).children.add(new Node(8));
+        root.children.get(2).children.get(0).children = new ArrayList<>();
+        root.children.get(2).children.get(0).children.add(new Node(12));
+        root.children.get(3).children = new ArrayList<>();
+        root.children.get(3).children.add(new Node(9));
+        root.children.get(3).children.add(new Node(10));
+        root.children.get(3).children.get(0).children = new ArrayList<>();
+        root.children.get(3).children.get(0).children.add(new Node(13));
+
+        s.postorder(root);
+        List<Integer> ls = s.postorder(root);
+        Integer[] ints = new Integer[ls.size()];
+        ints = ls.toArray(ints);
+        Assert.assertArrayEquals(new Integer[]{2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1}, ints);
+    }
+
+    // [8,null,1,null,8,5]
+    // Actual: [5,1,8]
+    // Expected: [8,5,1,8]
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        Node root = new Node(8);
+        root.children = new ArrayList<>();
+        root.children.add(new Node(1));
+        root.children.get(0).children = new ArrayList<>();
+        root.children.get(0).children.add(new Node(8));
+        root.children.get(0).children.add(new Node(5));
+
+        List<Integer> ls = s.postorder(root);
+        Integer[] ints = new Integer[ls.size()];
+        ints = ls.toArray(ints);
+        Assert.assertArrayEquals(new Integer[]{8, 5, 1, 8}, ints);
     }
 }
