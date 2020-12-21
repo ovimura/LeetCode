3dddbaf16c745a159f77929bdc494abf057ae92e
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2020-12-20 21:49:41 -0800

    adding solutionfor the DI String Match problem

--- src/main/java/DIStringMatch/Solution.java
+++ src/main/java/DIStringMatch/Solution.java
@@ -1,21 +1,25 @@
 package DIStringMatch;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
     public int[] diStringMatch(String S) {
         List<Integer> ints = new ArrayList<>();
-        int[] rs = new int[S.length()];
-        for(int i=0; i<S.length(); i++) {
+        int[] rs = new int[S.length()+1];
+        for(int i=0; i<=S.length(); i++) {
             ints.add(i);
         }
-        for(int j=0; j<S.length(); j++) {
+        int j = 0;
+        for(j=0; j<S.length(); j++) {
             if(S.charAt(j) == 'I') {
-
+                rs[j] = ints.remove(0);
+            } else if(S.charAt(j) == 'D') {
+                int last_idx = ints.size()-1;
+                rs[j] = ints.remove(last_idx);
             }
         }
+        rs[j] = ints.remove(0);
         return rs;
     }
 }
