43494934488c7865b77f33069a934ac70ba230b2
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2020-12-20 21:56:42 -0800

    adding solution for SearchInABinarySearchTree

---
+++ src/main/java/SearchInABinarySearchTree/Solution.java
@@ -0,0 +1,8 @@
+package SearchInABinarySearchTree;
+
+public class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        return null;
+    }
+}
---
+++ src/test/java/searchInABinarySearchTree/SolutionTest.java
@@ -0,0 +1,12 @@
+package searchInABinarySearchTree;
+
+import SearchInABinarySearchTree.Solution;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+    }
+}
