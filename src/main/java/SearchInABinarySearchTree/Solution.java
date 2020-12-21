040f115624c0d425352f937de64478b3a2855e89
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2020-12-21 09:28:56 -0800

    adding solution to the problem - 700. Search in a Binary Search Tree

--- src/main/java/SearchInABinarySearchTree/Solution.java
+++ src/main/java/SearchInABinarySearchTree/Solution.java
@@ -2,7 +2,25 @@ package SearchInABinarySearchTree;
 
 public class Solution {
 
+    public TreeNode tree = null;
+
     public TreeNode searchBST(TreeNode root, int val) {
-        return null;
+        if(root == null)
+            return root;
+        searchBST(root.left, val);
+        searchBST(root.right, val);
+        if(root.val == val) {
+            tree = root;
+        }
+        return tree;
+    }
+
+    public TreeNode traverse(TreeNode n) {
+        if(n == null)
+            return n;
+        traverse(n.left);
+        System.out.print(n.val);
+        traverse(n.right);
+        return n;
     }
 }
--- src/test/java/searchInABinarySearchTree/SolutionTest.java
+++ src/test/java/searchInABinarySearchTree/SolutionTest.java
@@ -1,6 +1,7 @@
 package searchInABinarySearchTree;
 
 import SearchInABinarySearchTree.Solution;
+import SearchInABinarySearchTree.TreeNode;
 import org.junit.Test;
 
 public class SolutionTest {
@@ -8,5 +9,12 @@ public class SolutionTest {
     @Test
     public void test1() {
         Solution s = new Solution();
+        TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7));
+        TreeNode n = s.searchBST(root, 2);
+        System.out.println(n.val);
+        System.out.println(s.tree.val);
+
+        s.traverse(s.tree);
     }
 }
