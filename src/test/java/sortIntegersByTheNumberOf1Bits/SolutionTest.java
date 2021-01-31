e4f8d2e3d6407df88ea07ecdfab718dce3918f02
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-01-31 14:30:42 -0800

    adding test scenarios

--- src/test/java/sortIntegersByTheNumberOf1Bits/SolutionTest.java
+++ src/test/java/sortIntegersByTheNumberOf1Bits/SolutionTest.java
@@ -16,4 +16,22 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertArrayEquals(new int[]{1,2,4,8,16,32,64,128,256,512,1024}, s.sortByBits(new int[]{1024,512,256,128,64,32,16,8,4,2,1}));
     }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{10000,10000}, s.sortByBits(new int[]{10000,10000}));
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{2,3,5,17,7,11,13,19}, s.sortByBits(new int[]{2,3,5,7,11,13,17,19}));
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        Assert.assertArrayEquals(new int[]{10,100,10000,1000}, s.sortByBits(new int[]{10,100,1000,10000}));
+    }
 }
