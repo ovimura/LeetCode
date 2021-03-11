a77d9aa99eb89d54994b13ddbb56e62dbad645a1
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-03-10 23:06:18 -0800

    removing subsequence palindrome

--- src/main/java/RemovePalindromicSubsequences/Solution.java
+++ src/main/java/RemovePalindromicSubsequences/Solution.java
@@ -3,35 +3,19 @@ package RemovePalindromicSubsequences;
 import java.util.Stack;
 
 public class Solution {
-    public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length()-1;
-        while (i<j) {
-            if(s.charAt(i) != s.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     public int removePalindromeSub(String s) {
-        int steps = 0;
-        int i = 0;
-        int j = s.length()-1;
-        String tmp = s;
-        while (tmp != "") {
-            if(i==j) {
-                tmp =
+        if (s.length() == 0)
+            return 0;
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left += 1;
+                right -= 1;
             }
-            if(isPalindrome(tmp.substring(i,j))) {
-                tmp = tmp.substring(i,j);
-                steps++;
-                i = 0;
-                j = tmp.length()-1;
-            }
-            j--;
+            else
+                return 2;
         }
-        return steps;
+        return 1;
     }
 }
