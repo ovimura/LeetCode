857cb481ce32be957d72e5a9a2ec1f25a45ce8cb
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-03-22 23:56:53 -0700

    adding solution for problem - InvertBinaryTree using Iterative Solution

--- src/main/java/InvertBinaryTree/Solution.java
+++ src/main/java/InvertBinaryTree/Solution.java
@@ -1,5 +1,8 @@
 package InvertBinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     public void print(TreeNode root) {
@@ -17,6 +20,21 @@ public class Solution {
         TreeNode r = invertTree(root.right);
         root.left = r;
         root.right = l;
+        return root;
+    }
+
+    public TreeNode invert_tree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
         return root;
     }
 }
--- src/test/java/invertBinaryTree/SolutionTest.java
+++ src/test/java/invertBinaryTree/SolutionTest.java
@@ -17,5 +17,8 @@ public class SolutionTest {
     @Test
     public void test2() {
         Solution s = new Solution();
+        TreeNode root = s.invert_tree(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9))));
+        s.print(root);
     }
 }
