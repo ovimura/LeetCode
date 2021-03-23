3768730db422e26a308f1aff7952dfced3e7b70b
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-03-23 01:05:27 -0700

    adding solution for the problem - Minimum Abs Difference

--- src/main/java/MinimumAbsoluteDifference/Solution.java
+++ src/main/java/MinimumAbsoluteDifference/Solution.java
@@ -41,4 +41,53 @@ public class Solution {
         });
         return paris;
     }
+
+    public List<List<Integer>> minimumAbsDifference1(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> pairs = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                int diff = Math.abs(arr[i] - arr[j]);
+                if(diff<min)
+                    min = diff;
+            }
+        }
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                int diff = Math.abs(arr[i] - arr[j]);
+                if(diff == min) {
+                    List<Integer> vals = new ArrayList<>();
+                    vals.add(arr[i]);
+                    vals.add(arr[j]);
+                    pairs.add(vals);
+                }
+            }
+        }
+        Collections.sort(pairs, new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.get(0)<o2.get(0)?-1:o1.get(0)>o2.get(0)?1:0;
+            }
+        });
+        return pairs;
+    }
+
+    public List<List<Integer>> minimumAbsDifference2(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> pairs = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<arr.length-1; i++) {
+            min = Math.min(min, Math.abs(arr[i] - arr[i+1]));
+        }
+        for(int i=0; i<arr.length; i++) {
+            if(Math.abs(arr[i] - arr[i+1]) == min) {
+                List<Integer> vals = new ArrayList<>();
+                vals.add(arr[i]);
+                vals.add(arr[i+1]);
+                pairs.add(vals);
+            }
+        }
+        return pairs;
+    }
 }
