b838fa0051073204f0a407c417971088955f0876
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-05-01 15:55:59 -0700

    adding solution for problem - Divisor Game

---
+++ src/main/java/DivisorGame/Solution.java
@@ -0,0 +1,37 @@
+package DivisorGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public boolean divisorGame(int n) {
+        int i = 1;
+        boolean Amove = false;
+        boolean Bmove = false;
+        while(true) {
+            Amove = false;
+            for (i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    Amove = true;
+                    break;
+                }
+            }
+            if(Amove == false)
+                return false;
+            n = n - i;
+            Bmove = false;
+            for (i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    Bmove = true;
+                    break;
+                }
+            }
+            n = n - i;
+            if(Bmove == false && Amove == true) {
+                return true;
+            } else if(Bmove == false && Amove == false) {
+                return false;
+            }
+        }
+    }
+}
