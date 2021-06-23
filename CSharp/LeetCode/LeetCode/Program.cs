b29af1cce1eb20bb3bf85a7e444e83210dfbdfb2
Ovidiu Mura <ovidiu.mura@yahoo.com>
2021-06-22 17:41:20 -0700

    adding solution for problem 1720

--- CSharp/LeetCode/LeetCode/Program.cs
+++ CSharp/LeetCode/LeetCode/Program.cs
@@ -32,14 +32,39 @@ namespace P1603 {
     }
 }
 
+namespace P1720 {
+    public class Solution
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            int[] rs = new int[encoded.Length+1];
+            rs[0] = first;
+            for (int i = 0; i < encoded.Length; i++) {
+                rs[i + 1] = encoded[i] ^ rs[i];
+            }
+            return rs;
+        }
+    }
+}
+
 
 namespace LeetCode
 {
     class Program
     {
+        static void testP1603() {
+            P1603.ParkingSystem p = new P1603.ParkingSystem(1, 1, 0);
+            Console.WriteLine(p.AddCar(1));
+            Console.WriteLine(p.AddCar(2));
+            Console.WriteLine(p.AddCar(3));
+            Console.WriteLine(p.AddCar(1));
+        }
         static void Main(string[] args)
         {
-            
+            testP1603();
+            int[] temp = new P1720.Solution().Decode(new int[] { 1, 2, 3 }, 1);
+            foreach(var t in temp)
+                Console.WriteLine(t);
         }
     }
 }
