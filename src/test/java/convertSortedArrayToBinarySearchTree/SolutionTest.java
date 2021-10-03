8a92dbdf1a3c4ca146ddc8aebcdf57d198c904ef
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-10-03 10:20:24 -0700

    adding solution refactorization

--- src/test/java/convertSortedArrayToBinarySearchTree/SolutionTest.java
+++ src/test/java/convertSortedArrayToBinarySearchTree/SolutionTest.java
@@ -2,6 +2,7 @@ package convertSortedArrayToBinarySearchTree;
 
 import ConvertSortedArrayToBinarySearchTree.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
@@ -9,13 +10,13 @@ public class SolutionTest {
     public void test1() {
         Solution s = new Solution();
         TreeNode tn = s.sortedArrayToBST(new int[]{-10,-3,0,5,9});
-        s.printPreOrder(tn);
+        //s.printPreOrder(tn);
     }
 
     @Test
     public void test2() {
         Solution s = new Solution();
         TreeNode tn = s.sortedArrayToBST(new int[]{1,2,3,4,5,6,7});
-        s.printPreOrder(tn);
+        //s.printPreOrder(tn);
     }
 }
