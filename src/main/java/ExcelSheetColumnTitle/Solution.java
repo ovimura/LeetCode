5675fcfcc2d5c23de833ce3b4fc623a4335238cf
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-10-17 22:32:17 -0700

    adding solution for 168. Excel Sheet Column Title

--- src/main/java/ExcelSheetColumnTitle/Solution.java
+++ src/main/java/ExcelSheetColumnTitle/Solution.java
@@ -49,4 +49,17 @@ public class Solution {
 //            sb.append(alphabet[columnNumber-1]);
 //        return sb.toString();
     }
+
+
+    public String convertToTitle1(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0) {
+            columnNumber--;
+            char c = (char)((columnNumber % 26) + 'A');
+            columnNumber /= 26;
+            sb.append(c);
+        }
+        sb.reverse();
+        return sb.toString();
+    }
 }
