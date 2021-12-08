bcada31def1e552a814eb5bf95f22c4dea412c6e
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-12-08 14:37:32 -0800

    adding solution for problem 228. Summary Ranges

--- src/main/java/SummaryRanges/Solution.java
+++ src/main/java/SummaryRanges/Solution.java
@@ -5,6 +5,30 @@ import java.util.List;
 
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
-        return new ArrayList();
+        List<String> rs = new ArrayList<>();
+        int start;
+        if(nums.length == 0)
+            return rs;
+        else if(nums.length == 1) {
+            rs.add(""+nums[0]);
+            return rs;
+        }
+        start = nums[0];
+        for(int i=0; i<nums.length-1; i++) {
+            if(i>0 && nums[i-1] != nums[i]-1)
+                start = nums[i];
+            if(nums[i+1] == nums[i]+1 && (nums.length-1) == i+1) {
+                rs.add(start+"->"+nums[i+1]);
+            } else if(nums[i+1] == nums[i]+1) {
+                continue;
+            } else if(start == nums[i]) {
+                rs.add(""+start);
+            } else {
+                rs.add(start+"->"+nums[i]);
+            }
+            if((i+1) == (nums.length-1) && nums[i]+1 != nums[i+1])
+                rs.add(""+nums[i+1]);
+        }
+        return rs;
     }
 }
--- src/test/java/summaryRanges/SolutionTest.java
+++ src/test/java/summaryRanges/SolutionTest.java
@@ -12,6 +12,34 @@ public class SolutionTest {
     public void test1() {
         Solution s = new Solution();
         List<String> r = s.summaryRanges(new int[]{});
-        Assert.assertEquals(0, r.size());
+        Assert.assertArrayEquals(new String[]{}, r.toArray());
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        List<String> r = s.summaryRanges(new int[]{1});
+        Assert.assertArrayEquals(new String[]{"1"}, r.toArray());
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        List<String> r = s.summaryRanges(new int[]{-1});
+        Assert.assertArrayEquals(new String[]{"-1"}, r.toArray());
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        List<String> r = s.summaryRanges(new int[]{0,2,3,4,6,8,9});
+        Assert.assertArrayEquals(new String[]{"0","2->4","6","8->9"}, r.toArray());
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        List<String> r = s.summaryRanges(new int[]{0,1,2,4,5,7});
+        Assert.assertArrayEquals(new String[]{"0->2","4->5","7"}, r.toArray());
     }
 }
