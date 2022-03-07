115d985e80ff5fb71ad433262e664ed31138c9a0
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-03-07 14:29:36 -0800

    adding solution for problem - 783. Minimum Distance Between BST Nodes

---
+++ src/main/java/MinimumDistanceBetweenBSTNodes/Solution.java
@@ -0,0 +1,22 @@
+package MinimumDistanceBetweenBSTNodes;
+
+public class Solution {
+    int min = Integer.MAX_VALUE;
+    int prev = Integer.MAX_VALUE;
+    public int minDiffInBST(TreeNode root) {
+        traverse(root);
+        return min;
+    }
+
+    void traverse(TreeNode r) {
+        if(r == null)
+            return;
+        traverse(r.left);
+
+        if(prev != Integer.MAX_VALUE)
+            min = Math.min(Math.abs(r.val - prev), min);
+        prev = r.val;
+
+        traverse(r.right);
+    }
+}
