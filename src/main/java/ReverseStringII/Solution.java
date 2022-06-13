158fb3db00174430ee84ce1e022b10fbf0e62d34
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-06-12 18:27:46 -0700

    adding version using my version of reverse function

--- src/main/java/ReverseStringII/Solution.java
+++ src/main/java/ReverseStringII/Solution.java
@@ -25,4 +25,34 @@ public class Solution {
         }
         return s;
     }
+
+    public String reverseStr2(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        if(k > s.length())
+            return reverse(sb.toString());
+        else if (s.length() < 2*k && s.length() >= k){
+            return reverse(sb.substring(0,k)) + sb.substring(k);
+        } else if ((s.length() % (2*k)) == 0) {
+            for(int i=0; i<s.length(); i+=(2*k)) {
+                s = s.substring(0, i) + reverse(s.substring(i, i+k)) + s.substring(i+k);
+            }
+        } else if (2*k % s.length() != 0) {
+            int i = 0;
+            for(; i<(s.length()-(s.length()%k)); i+=(2*k)) {
+                s= s.substring(0, i) + reverse(s.substring(i, i+k)) + s.substring(i+k);
+            }
+            if(i < s.length()) {
+                s = s.substring(0, i) + reverse(s.substring(i));
+            }
+        }
+        return s;
+    }
+
+    String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); i++) {
+            sb = new StringBuilder(s.charAt(i)+sb.toString());
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/reverseStringII/SolutionTest.java
+++ src/test/java/reverseStringII/SolutionTest.java
@@ -10,19 +10,29 @@ public class SolutionTest {
     public void test1() {
         Solution s = new Solution();
         Assert.assertEquals("sss", s.reverseStr("sss", 2));
+        Assert.assertEquals("sss", s.reverseStr2("sss", 2));
     }
 
     @Test
     public void test2() {
         Solution s = new Solution();
         Assert.assertEquals("iuuijuuiabcd", s.reverseStr("uiuiujuibacd", 2));
+        Assert.assertEquals("iuuijuuiabcd", s.reverseStr2("uiuiujuibacd", 2));
         Assert.assertEquals("va", s.reverseStr("av", 2));
+        Assert.assertEquals("va", s.reverseStr2("av", 2));
     }
 
     @Test
     public void test3() {
         Solution s = new Solution();
         Assert.assertEquals("cabdzthr", s.reverseStr("bacdztrh", 3));
+        Assert.assertEquals("cabdzthr", s.reverseStr2("bacdztrh", 3));
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        Assert.assertEquals("cabdzthr", s.reverseStr2("bacdztrh", 3));
     }
 
 }
