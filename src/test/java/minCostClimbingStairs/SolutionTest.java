ae4e2b28f8b4037054b9786b0163f8439be5b29c
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-11-06 10:19:30 -0800

    adding solution for problem - Min Cost Climbing Stairs

---
+++ src/test/java/minCostClimbingStairs/SolutionTest.java
@@ -0,0 +1,14 @@
+package minCostClimbingStairs;
+
+import MinCostClimbingStairs.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+        Assert.assertEquals(15, s.minCostClimbingStairs(new int[]{10,15,20}));
+    }
+}
