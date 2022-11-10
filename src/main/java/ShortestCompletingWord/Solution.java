d0b0626c83a2465abcd23eeaa20a6ca7ee3eccb1
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-11-10 11:43:54 -0800

    adding solution for problem - Shortest Completing Word

--- src/main/java/ShortestCompletingWord/Solution.java
+++ src/main/java/ShortestCompletingWord/Solution.java
@@ -1,9 +1,12 @@
 package ShortestCompletingWord;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Solution {
-    public String shortestCompletingWord(String licensePlate, String[] words) {
+    public String shortestCompletingWord_orig(String licensePlate, String[] words) {
         HashMap<Character, Integer> m = new HashMap<Character, Integer>();
         int licensePlateSize = licensePlate.length();
         for(int i=0;i<licensePlateSize;i++){
@@ -33,5 +36,52 @@ public class Solution {
             }
         }
         return answer;
+    }
+
+
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<licensePlate.length(); i++) {
+            char c = Character.toLowerCase(licensePlate.charAt(i));
+            if((c <= 'z' && c >= 'a' || c >= 'A' && c <= 'Z') && map.get(c) == null) {
+                map.put(Character.toLowerCase(c), 1);
+            } else if (c <= 'z' && c >= 'a' || c >= 'A' && c <= 'Z'){
+                int v = map.get(Character.toLowerCase(c))+1;
+                map.put(c, v);
+            }
+        }
+        List<String> slist = new ArrayList<>();
+        for(String w: words) {
+            Map<Character, Integer> wmap = new HashMap<>();
+            for(int k=0; k<w.length(); k++) {
+                char c = w.charAt(k);
+                c = Character.toLowerCase(c);
+                if(c <= 'z' && c >= 'a' && wmap.get(c) == null) {
+                    wmap.put(c, 1);
+                } else if (c <= 'z' && c >= 'a'){
+                    int v = wmap.get(c)+1;
+                    wmap.put(c, v);
+                }
+            }
+            boolean f = false;
+            int siz = map.keySet().size();
+            int z = 0;
+            for(char k: map.keySet()) {
+                if(wmap.get(k) != null && wmap.get(k)<map.get(k)) {
+                    f = true;
+                } else if (wmap.get(k) != null && wmap.get(k)>=map.get(k)) {
+                    z++;
+                }
+            }
+            if(!f && siz == z)
+                slist.add(w);
+            wmap.clear();
+        }
+        String shortest_word = slist.get(0);
+        for(String s: slist) {
+            if(s.length()<shortest_word.length())
+                shortest_word = s;
+        }
+        return shortest_word;
     }
 }
--- src/test/java/shortestCompletingWord/SolutionTest.java
+++ src/test/java/shortestCompletingWord/SolutionTest.java
@@ -11,4 +11,10 @@ public class SolutionTest {
         Solution s = new Solution();
         Assert.assertEquals("steps", s.shortestCompletingWord("1s3 PSt", new String[]{"step","steps","stripe","stepple"}));
     }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        Assert.assertEquals("husband", s.shortestCompletingWord("Ah71752", new String[]{"suggest","letter","of","husband","easy","education","drug","prevent","writer","old"}));
+    }
 }
