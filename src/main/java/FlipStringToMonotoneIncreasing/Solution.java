59e857cdc285516c876853d3c15ebd80578b58f2
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2023-01-16 21:55:11 -0800

    refactoring

--- src/main/java/FlipStringToMonotoneIncreasing/Solution.java
+++ src/main/java/FlipStringToMonotoneIncreasing/Solution.java
@@ -1,5 +1,6 @@
 package FlipStringToMonotoneIncreasing;
 
+// Reference: https://medium.com/@smartsplash/flip-string-to-monotone-increasing-in-o-n-time-b9242fe2a4fb
 public class Solution {
     public int minFlipsMonoIncr(String s) {
         int oneC = 0, flipC = 0;
