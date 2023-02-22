3307588174aaf791242d57c2ced24e36316fa35d
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2023-02-22 11:41:56 -0800

    adding solution for problem Find Center Of Star Graph

--- py/sol/P1791-FindCenterOfStarGraph.py
+++ py/sol/P1791-FindCenterOfStarGraph.py
@@ -9,13 +9,13 @@ class Solution:
             if e[0] not in dic.keys():
                 dic[e[0]] = [e[1]]
             else:
-                v = edges[e[0]]
+                v = dic[e[0]]
                 v.append(e[1])
                 dic[e[0]] = v
             if e[1] not in dic.keys():
                 dic[e[1]] = [e[0]]
             else:
-                v = edges[e[1]]
+                v = dic[e[1]]
                 v.append(e[0])
                 dic[e[1]] = v
         n = len(dic.keys())
@@ -29,4 +29,8 @@ s = Solution()
 
 
 assert 2 == (s.findCenter([[1, 2], [2, 3], [4, 2]]))
+
+t = s.findCenter([[1,16],[16,2],[3,16],[4,16],[16,5],[16,6],[7,16],[16,8],[9,16],[10,16],[16,11],[12,16],[16,13],[16,14],[15,16],[16,17]])
+
+assert 16 == t
 
