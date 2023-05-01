841a16c45253c584c31c331acae76e987f813ccf
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2023-05-01 09:58:30 -0700

    refactoring

--- py/sol/Permutation.py
+++ py/sol/Permutation.py
@@ -1,13 +1,13 @@
 from typing import List
 
 
-class Combinations:
+class Permutation:
     """
         C(n,k) = n!/k!(n-k)!
         P(n,k) = n!/(n-k)!
     """
 
-    def comb_of_n_token_k(self, combinations, data, i, k, combination, z) -> List[List[int]]:
+    def perm_of_n_token_k(self, combinations, data, i, k, combination, z) -> List[List[int]]:
         # if i == len(combination):
         if i == k:
             cp = combination.copy()
@@ -15,13 +15,15 @@ class Combinations:
         else:
             for j in range(i, len(data)):
                 combination[i], combination[j] = combination[j], combination[i]
-                self.comb_of_n_token_k(combinations, data, i+1, k, combination, i)
+                self.perm_of_n_token_k(combinations, data, i+1, k, combination, i)
                 combination[i], combination[j] = combination[j], combination[i]
         return combinations
 
 
-c = Combinations()
+c = Permutation()
 temp = []
-c.comb_of_n_token_k(temp, [1, 2, 3, 4], 0, 2, [1, 2, 3, 4], 0)
+# c.perm_of_n_token_k(temp, [1, 2, 3, 4], 0, 3, [1, 2, 3, 4], 0)
+c.perm_of_n_token_k(temp, [2, 2, 8, 8, 2], 0, 3, [2, 2, 8, 8, 2], 0)
 for t in temp:
     print(t)
+
