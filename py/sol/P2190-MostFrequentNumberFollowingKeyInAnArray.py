275f10a66972140fd3e99a7707b6448cc31fca66
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2023-06-18 15:44:46 -0700

    adding solution for problem - P2190

--- py/sol/P2190-MostFrequentNumberFollowingKeyInAnArray.py
+++ py/sol/P2190-MostFrequentNumberFollowingKeyInAnArray.py
@@ -5,7 +5,7 @@ class Solution:
     def mostFrequent(self, nums: List[int], key: int) -> int:
         r = 0
         freq = {}
-        for i in range(len(nums)-2):
+        for i in range(len(nums)-1):
             if nums[i] == key:
                 if nums[i+1] not in freq.keys():
                     freq[nums[i+1]] = 1
@@ -24,3 +24,5 @@ s = Solution()
 
 assert 100 == (s.mostFrequent([1, 100, 200, 1, 100], 1))
 assert 2 == (s.mostFrequent([2, 2, 2, 2, 3], 2))
+assert 2 == (s.mostFrequent([1, 1000, 2], 1000))
+
