6b0e2cf6236a1db37dbb2a093fc2a35f71bd0bf5
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-02-24 20:08:28 -0800

    adding formatting spaces

--- py/sol/P3838-WeightedWordMapping.py
+++ py/sol/P3838-WeightedWordMapping.py
@@ -1,8 +1,8 @@
 from typing import List
 
 
-
 class Solution:
+
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
         ww = []
         for w in words:
@@ -22,7 +22,9 @@ class Solution:
 s = Solution()
 
 assert "rij" == s.mapWordWeights(["abcd", "def", "xyz"],
-                                 [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2])
+                                 [5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2])
 
-assert "onhpnpevpaaxkkab" == (s.mapWordWeights(["ojibmbsqwf","nmcksaj","vinnwxscy","mweijrtyin","lqxxg","d","ykqyc","cqlzt","gimfdpp","rnvaqe","ajkaraz","aoceuywyxq","g","g","pmshdzobpw","wb"],
-                       [22,14,17,36,47,23,15,44,30,30,45,16,18,17,48,35,13,40,19,36,45,16,36,36,12,26]))
+assert "onhpnpevpaaxkkab" == (s.mapWordWeights(
+    ["ojibmbsqwf", "nmcksaj", "vinnwxscy", "mweijrtyin", "lqxxg", "d", "ykqyc", "cqlzt", "gimfdpp", "rnvaqe", "ajkaraz",
+     "aoceuywyxq", "g", "g", "pmshdzobpw", "wb"],
+    [22, 14, 17, 36, 47, 23, 15, 44, 30, 30, 45, 16, 18, 17, 48, 35, 13, 40, 19, 36, 45, 16, 36, 36, 12, 26]))
